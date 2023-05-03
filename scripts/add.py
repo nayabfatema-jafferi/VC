3bac8d6696ab5b3e75b782b554d86c7527079700
nayabfatema-jafferi <nayabfatema@gmail.com>
2023-05-03 15:18:43 +0400

    Sport name Chnaged

--- scripts/add.py
+++ scripts/add.py
@@ -23,8 +23,8 @@ def write_json(data,filename = '../response.json'):
 def call_sport():
     name = input("Please add your name: ")
     sport = input("Please add your favourite sports name: ")
-    if (sport == "Basketball"):
-        sport = 'Cricket'
+    if (sport == ""):
+        sport = 'Badminton'
     if (name):
         response[name] = sport.lower()
         write_json(response)
